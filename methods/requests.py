f1a469fe9770038b1a8a64c8d820fb661fe6c22c
ztelliot <ztell@foxmail.com>
2022-09-01 00:57:46 +0800

    fix: restore urllib3 after requests

--- methods/requests.py
+++ methods/requests.py
@@ -9,31 +9,32 @@ import IPy
 class Requests(MethodBaseType):
     @staticmethod
     def family_v4():
-        family = socket.AF_INET
-        return family
+        return socket.AF_INET
 
     @staticmethod
     def family_v6():
-        family = socket.AF_INET6
-        return family
+        return socket.AF_INET6
 
     @staticmethod
     def getip(version: int = 4, interface: str = "", config: dict = None) -> list:
+        ragf = urllib3_cn.allowed_gai_family
         urllib3_cn.allowed_gai_family = Requests.family_v6 if version == 6 else Requests.family_v4
         if interface:
             inips = Interface.getip(version, interface)
             if inips:
-                rcc = requests.packages.urllib3.util.connection.create_connection
-                def set_src_addr(address, timeout, *args, **kw):
+                rcc = urllib3_cn.create_connection
+                def set_src(address, timeout, source_address = None, socket_options = None):
                     source_address = (inips[0], 0)
-                    return rcc(address, timeout=timeout, source_address=source_address)
-                urllib3_cn.create_connection = set_src_addr
+                    return rcc(address, timeout, source_address, socket_options)
+                urllib3_cn.create_connection = set_src
             else:
+                urllib3_cn.allowed_gai_family = ragf
                 return []
         if config and 'url' in config:
             urls = config['url'] if isinstance(config['url'], list) else [config['url']]
         else:
             urls = ['https://api.ipify.org?format=json', 'https://api.myip.la', 'https://api.ip.sb/ip']
+        res = []
         for url in urls:
             try:
                 ret = requests.get(url, timeout=3)
@@ -42,9 +43,11 @@ class Requests(MethodBaseType):
                 except:
                     ip = ret.text
                 if ip and IPy.IP(ip).version() == version:
-                    return [ip]
-                else:
-                    return []
+                    res = [ip]
+                    break
             except:
                 continue
-        return []
+        urllib3_cn.allowed_gai_family = ragf
+        if interface and inips:
+            urllib3_cn.create_connection = rcc
+        return res
